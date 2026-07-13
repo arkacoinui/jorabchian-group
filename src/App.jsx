b1db32c77fb626a485176a831edd57c8201ecee1
arkacoinui <arkacoin.ui@gmail.com>
2026-07-13 02:34:24 -0700

    fix: clean all remaining merge conflicts across the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import AboutMe from './components/AboutMe.jsx';
@@ -29,10 +28,4 @@ export default function App() {
       <Footer />
     </div>
   );
-=======
-import LandingPage from './components/LandingPage'
-
-export default function App() {
-  return <LandingPage />
->>>>>>> 9ab1f09abd0a8128c6217b3631fd95b16ed88210
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -9,15 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 9ab1f09abd0a8128c6217b3631fd95b16ed88210
